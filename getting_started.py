01c19126915b731c1e652771f6d586518121d62d
chandiniperuri <chandini.peruri@gmail.com>
2020-06-16 09:58:13 -0700

    added get_tables logic in utils and used in driver program

--- getting_started.py
+++ getting_started.py
@@ -1,13 +1,18 @@
 """this is the driver program"""
 import sys
 from config import DB_DETAILS
+from utils import get_tables
+
 
 def main():
-    print(len(sys.argv))
-    if(len(sys.argv)<2):
+    if len(sys.argv) < 2:
         print("Usage: python <filename> <env>")
     env = sys.argv[1]
     db_details = DB_DETAILS[env]
-    print(db_details)
+    tables = get_tables('table_list.txt')
+    for table in tables['table_name']:
+        print(table)
+
+
 if __name__ == '__main__':
     main()
